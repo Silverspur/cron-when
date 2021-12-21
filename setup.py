0d6c0f554751f411c6446882ea0897bf1f2eab39
Silverspur <8577610+Silverspur@users.noreply.github.com>
2021-12-21 17:33:30 +0100

    Fix errors and add information to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 from setuptools import setup, find_packages
 
-setup(name="cronnext",
-      version="0.0.1",
-      description="A library computing next occurrence of a cron pattern",
-      author="Charles Lumet",
-      author_email="charles.lumet@gmail.com",
-      packages=find_packages(),
-      install_requires=[],
-      license="GNU General Public License v3.0")
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    setup(
+        name="cronwhen",
+        version="0.0.1",
+        description="A library computing next occurrence of a cron pattern",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Charles Lumet",
+        author_email="charles.lumet@gmail.com",
+        packages=find_packages(),
+        install_requires=[],
+        license="GNU General Public License v3.0",
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Topic :: System"
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ]
+    )
